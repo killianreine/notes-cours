358eb91fb2467d6e95eab9e00933b68a8df875ee
Killian Reine <109869909+killianreine@users.noreply.github.com>
2025-10-23 09:54:49 +0200

    Update quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Killian Reine",
+    pageTitle: "Killian REINE",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
